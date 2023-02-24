6e8cbfb783c155c2d676df59bbe044b13c7d3975
Gino Tateo <47284697+GinoTateo@users.noreply.github.com>
2023-02-23 19:20:04 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+
+@app.route('/projects')
+def resume():
+    return render_template('projects.html')
+
+
+if __name__ == '__main__':
+    app.run()
